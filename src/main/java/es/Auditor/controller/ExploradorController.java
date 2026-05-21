01c09e67f4c8f91e41ff136a8c0b0309ef5ac322
Abdelhadi <taalib0101@gmail.com>
2026-05-21 13:25:56 +0200

    añadido vista.prinxipal.fxml y su controlador y vista_explorador.fxml

---
+++ src/main/java/es/Auditor/controller/ExploradorController.java
@@ -0,0 +1,9 @@
+package es.auditor.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ExploradorController {
+    @FXML
+    private TextField txtFiltroEmpresa;
+}
--- src/main/java/es/Auditor/controller/MainController.java
+++ src/main/java/es/Auditor/controller/MainController.java
@@ -1,26 +1,13 @@
 package es.auditor.controller;
 
+import java.io.*;
+
+import es.auditor.util.GestorArchivos;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
 
 public class MainController {
-
-    // Botones del menú lateral
-    @FXML
-    private Button btnDashboard;
-    
-    @FXML
-    private Button btnExplorador;
-    
-    @FXML
-    private Button btnMapa;
-    
-    @FXML
-    private Button btnImportar;
-    
-    @FXML
-    private Button btnExportar;
 
     // Nuestro contenedor dinámico (el "lienzo")
     @FXML
@@ -32,4 +19,40 @@ public class MainController {
         System.out.println("Interfaz principal cargada correctamente.");
          
     }
+
+    @FXML
+    public void abrirExploradorArchivos() {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Seleccionar archivo CSV");
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Archivos CSV", "*.csv"));
+
+        File archivoSeleccionado = fc.showOpenDialog(null);
+
+        if (archivoSeleccionado != null) {
+            String ruta = archivoSeleccionado.getAbsolutePath();
+            GestorArchivos ga = new GestorArchivos();
+            ga.importarDatos(ruta);
+        } 
+
+    }
+
+    @FXML
+    public void mostrarDashboard() {
+        
+    }
+    @FXML
+    public void mostrarExplorador() {
+
+    }
+    @FXML
+    public void mostrarMapa() {
+
+    }
+    @FXML
+    public void generarInforme() {
+
+    }
+
+
+
 }
